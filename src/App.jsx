cb2beb2e0a110893ec73ab6045f62b0e9eb64b89
haris <harishammadpak1@gmail.com>
2025-03-10 13:15:10 +1400

    33

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import Login from "./page/login-page";
 import Register from "./page/regisret-page";
 import Todu from "./todu-page";
 import Error from "./Error-page";
-import {Routes,Route,BrowserRouter} from 'react-router-dom'
+import {Routes,Route,BrowserRouter,HashRouter} from 'react-router-dom'
 import { Authprovider } from "./store/authContaxt";
 import Admin from "./Admin";
 function App() {
   return <>
   
-  <BrowserRouter>
+  <HashRouter>
   <Authprovider>
   <Routes>
     <Route path="/" element={<Login/>}/>
@@ -18,7 +18,7 @@ function App() {
     <Route path="*" element={<Error/>}/>
   </Routes>
   </Authprovider>
-  </BrowserRouter>
+  </HashRouter>
   </>;
 }
 
